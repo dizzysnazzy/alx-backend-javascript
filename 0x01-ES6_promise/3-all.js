f4408e6f7a495059d58995ccaeb8ef5628aa775a
dizzysnazzy <dismaskipchumba2@gmail.com>
2022-04-05 10:45:58 -0700

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const [res1, res2] = await Promise.all([createUser(), uploadPhoto()]);
-    console.log(res2.body, res1.firstName, res1.lastName);
-  } catch (_) {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
